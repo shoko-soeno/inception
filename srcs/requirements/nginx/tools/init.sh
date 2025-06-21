7e6bf850a57d1cbee0cdcc3de6a3b1ceee547d15
Shoko Soeno <ssoeno@c6r2s6.42tokyo.jp>
2025-06-21 13:23:20 +0900

    not working

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -8,8 +8,9 @@ cd /etc/nginx/ssl
 
 # 秘密鍵作成(NISTなどのセキュリティ機関が2048bit以上を推奨しており、パフォーマンスバランスがいいためこちらを指定)
 openssl genrsa -out inception.key 2048
-# CSR(証明書署名要求リクエスト)作成
-openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=ssoeno.42.fr"
+# CSR(証明書署名要求リクエスト)作成 CNはCommon Nameの略で、証明書の所有者を識別するための名前
+# openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=ssoeno.42.fr"
+openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=localhost"
 # CSRから自己署名証明書を作成
 openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
 # # Create certificate
