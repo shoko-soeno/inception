a6ede9bbcfcdafe3baeff35501c1e495af020e22
Shoko Soeno <ssoeno@c6r2s6.42tokyo.jp>
2025-06-29 14:08:14 +0900

    rm comments

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -6,19 +6,14 @@ set -ex
 # SSL証明書の保存先移動
 cd /etc/nginx/ssl
 
-# 秘密鍵作成(NISTなどのセキュリティ機関が2048bit以上を推奨しており、パフォーマンスバランスがいいためこちらを指定)
-openssl genrsa -out inception.key 2048
+# 秘密鍵作成(NISTなどのセキュリティ機関が2048bit以上を推奨)
 # CSR(証明書署名要求リクエスト)作成 CNはCommon Nameの略で、証明書の所有者を識別するための名前
-# openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=ssoeno.42.fr"
-openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=localhost"
 # CSRから自己署名証明書を作成
-openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
-# # Create certificate
-# if [ ! -f inception.key ] || [ ! -f inception.crt ]; then
-#     openssl genrsa -out inception.key 2048
-#     openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=ssoeno.42.fr"
-#     openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
-# fi
+if [ ! -f inception.key ] || [ ! -f inception.crt ]; then
+    openssl genrsa -out inception.key 2048
+    openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=ssoeno.42.fr"
+    openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
+fi
 
 # Nginxをデーモンモードではなくフォアグラウンドで起動　※コンテナが終了しないようにする
 exec nginx -g 'daemon off;'
